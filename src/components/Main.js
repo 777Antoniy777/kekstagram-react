c5ee1078b00b09b7a4c7f57ddbdbb01be25b84ae
anton_kuzmitsky <tat7106@gmail.com>
2019-10-09 16:58:48 +0600

    split main component and  done ImgFilters component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import '../css/blocks/Main.css';
+import ImgFilters from './Img-filters';
 
 function Main() {
   return (
 
     <main className="Main">
 
-      {/* <!-- Фильтрация изображений от других пользователей --> */}
-      {/* убрал класс .Img-filters--inactive */}
-      <section className="Img-filters Container">
-        <h2 className="Img-filters__title visually-hidden">Фильтр фотографий</h2>
-        <form className="Img-filters__form" action="index.html" method="get" autoComplete="off">
-          <button type="button" className="Img-filters__button Img-filters__button--active" id="filter-popular">Популярные</button>
-          <button type="button" className="Img-filters__button" id="filter-new">Новые</button>
-          <button type="button" className="Img-filters__button" id="filter-discussed">Обсуждаемые</button>
-        </form>
-      </section>
+      <ImgFilters />
 
       {/* <!-- Контейнер для изображений от других пользователей --> */}
       <section className="Pictures  Container">
