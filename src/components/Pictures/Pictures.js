c5c9677326a9846992160591227f8fa6004852f8
anton_kuzmitsky <tat7106@gmail.com>
2019-10-10 18:29:12 +0600

    tried to create state of component

--- src/components/Pictures/Pictures.js
+++ src/components/Pictures/Pictures.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import './Pictures.css';
 import ImgUpload from '../ImgUpload/ImgUpload';
+class Pictures extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: 'https://js.dump.academy/kekstagram/data',
+      pictures: []
+    };
+  }
 
-function Pictures() {
-  return (
+  componentDidUpdate(prevProps) {
+    if(prevProps.myProps !== this.props.myProp) {
+      this.fetchData(this.props.url)
+        .then(response => response.json)
+        .then(pictures => console.log(pictures))
+      // const arr = fetch('https://js.dump.academy/kekstagram/data')
+      //   .then(response => response.json)
+      //   .then(pictures => console.log(pictures))
+      // console.log(arr);
 
-    // Контейнер для изображений от других пользователей
-    <section className="Pictures  container">
-      <h2 className="Pictures__title  visually-hidden">Фотографии других пользователей</h2>
+      // this.setState({
+      //   pictures: arr
+      // })пш
+    }
+  }
 
-      {/* <!-- Поле для загрузки нового изображения на сайт --> */}
-      <ImgUpload />
+  render() {
+    return (
 
-      {/* <!-- Здесь будут изображения других пользователей --> */}
+      // Контейнер для изображений от других пользователей
+      <section className="Pictures  container">
+        <h2 className="Pictures__title  visually-hidden">Фотографии других пользователей</h2>
 
-    </section>
-  );
+        {/* <!-- Поле для загрузки нового изображения на сайт --> */}
+        <ImgUpload />
+
+        {/* <!-- Здесь будут изображения других пользователей --> */}
+
+      </section>
+    );
+  }
 }
 
 export default Pictures;
