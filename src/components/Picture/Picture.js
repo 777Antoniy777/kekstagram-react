2d29cfcf28eb6580387d6aa5a6b4f9cd197ea73f
anton_kuzmitsky <tat7106@gmail.com>
2019-10-15 17:07:33 +0600

    replaced alt at pictures

--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Picture.css';
 
 const Picture = (props) => {
+  let pictureNumber = parseInt(props.url.split('/')[1]);
 
   return (
 
       <a href="#s" className="Picture" key={props.key}>
 
-        <img className="Picture__img" src={`/${props.url}`} width="182" height="182" alt={`Картинка ${parseInt(props.url.split('/')[1])}`}/>
+        <img className="Picture__img" src={`/${props.url}`} width="182" height="182" alt={`Картинка ${pictureNumber}`}/>
 
         <p className="Picture__info">
           <span className="Picture__comments">{props.comments.length}</span>
