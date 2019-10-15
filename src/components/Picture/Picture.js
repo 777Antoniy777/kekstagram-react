8e49b7935cf616a0c0b250b4535b3bf6036e9732
anton_kuzmitsky <tat7106@gmail.com>
2019-10-15 17:26:18 +0600

    added variables for src of pictures

--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './Picture.css';
 
 const Picture = (props) => {
-  let pictureNumber = parseInt(props.url.split('/')[1]);
+  const pictureNumber = parseInt(props.url.split('/')[1]);
+  const pictureSrc = `/${props.url}`;
 
   return (
 
       <a href="#s" className="Picture" key={props.key}>
 
-        <img className="Picture__img" src={`/${props.url}`} width="182" height="182" alt={`Картинка ${pictureNumber}`}/>
+        <img className="Picture__img" src={ pictureSrc } width="182" height="182" alt={`Картинка ${pictureNumber}`}/>
 
         <p className="Picture__info">
           <span className="Picture__comments">{props.comments.length}</span>
